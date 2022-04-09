4dfab5afb894a47eae22f6a48c3654b5cc94dcd0
Tracy Rothery <tracy.rothery@gmail.com>
2022-04-09 17:52:51 +0800

    commit_got_buttons_to_change_state_of_hand
    commit_got_buttons_to_change_state_of_hand

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+
+
+import "./App.css";
+
+function App() {
+    //user options rock,paper,sissors
+    const [userOption, setuserOption] = useState("")
+
+
+    //state
+    return (
+        <div className = "app">
+        <div className={`userimage ${userOption}`}> 
+        <h1>Rock, Paper, Scissors</h1>
+        <button onClick={() => setuserOption("paper")}>Paper</button>
+        <button onClick={() => setuserOption("scissors")}>Scissors</button>
+        <button onClick={() => setuserOption("rock")}>Rock</button>
+      </div>
+      </div>
+        
+ );
+    
+   
+   }
+   
+   export default App;
+   
